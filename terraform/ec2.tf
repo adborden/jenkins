0b724be3f441f59014ca68cefb3eee09f6eab10b
Aaron D Borden <adborden@a14n.net>
2018-05-26 23:41:40 -0700

    Remove debugging access

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -25,28 +25,6 @@ resource "aws_security_group" "jenkins" {
     self = true
   }
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # debugging
-  ingress = {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port = 0
     to_port = 0
@@ -60,8 +38,6 @@ resource "aws_instance" "jenkins" {
   ami           = "${data.aws_ami.jenkins.id}"
   instance_type = "t2.micro"
   subnet_id     = "${element(aws_subnet.jenkins.*.id, count.index)}"
-  key_name      = "default"
-  associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_vpc.jenkins.default_security_group_id}", "${aws_security_group.jenkins.id}"]
 
   tags {
